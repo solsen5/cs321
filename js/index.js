51e11abe2aa40d757fc55c00e346083bbe602f97
PTuLo <pturriagol@gmail.com>
2018-11-20 15:07:31 -0500

    playing around with js

--- js/index.js
+++ js/index.js
@@ -1,6 +1,9 @@
 var API_URL = "https://rvyl38zfdf.execute-api.us-east-1.amazonaws.com/dev"
 
-in_data = {"user": "password"}
+in_data = {
+	"user": ,
+	"password": 
+	  }
 in_date = JSON.stringy(in_data)
 $.ajax({
         type: JSON,
