1a01bd3acb7a8eb1aa76b004099bb99c0c52fac3
Samantha <solsen5@gmu.edu>
2018-11-20 14:51:16 -0500

    Added a javascript file for the index page

---
+++ js/index.js
@@ -0,0 +1,10 @@
+var API_URL = "https://rvyl38zfdf.execute-api.us-east-1.amazonaws.com/dev"
+
+in_data = {"user": "password"}
+in_date = JSON.stringy(in_data)
+$.ajax({
+        type: JSON,
+        url: API_URL,
+        data: in_data,
+        contentType: 'connection/json'
+        })
