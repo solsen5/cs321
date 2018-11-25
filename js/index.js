e25a1e6ea572035b9ad6e412ded470c93ee5a1ae
Samantha <solsen5@gmu.edu>
2018-11-25 15:18:54 -0500

    removed unused code

--- js/index.js
+++ js/index.js
@@ -5,25 +5,6 @@ var API_URL = "https://rvyl38zfdf.execute-api.us-east-1.amazonaws.com/prod/"
 //Turns off async so response from lambda can be used client-side
 jQuery.ajaxSetup({async:false});
 
-
-//currently not being used
-$(document).ready(function() {
-    $("#submit").on("click", function() {
-        console.log("dfsl");
-        in_data = {
-                    "user": "U",
-                    "password": "p"
-                  }
-        in_date = JSON.stringify(in_data)
-        $.ajax({
-                type: JSON,
-                url: API_URL,
-                data: in_data,
-                contentType: 'connection/json'
-                });
-    })
-});
-
 //validates the username and password
 function validate() {
         //data that will be sent the lambda through the api gateway
@@ -37,11 +18,10 @@ function validate() {
                 data: JSON.stringify(in_data),
                 contentType: 'application/json'
                 });
-        console.log(res["responseJSON"]['body'])
+        console.log(res)
     //lambda(server-side) validates
-    if(res["responseJSON"]['body'] === "validation succeeded")
+    if(res["responseJSON"] === "validation succeeded")
     {
-        alert( "validation succeeded" );
         location.href="profile.html";
     }
     else
