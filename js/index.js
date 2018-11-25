572a5af497ae67930d984801b8c1396c838c24f6
Samantha <solsen5@gmu.edu>
2018-11-25 14:27:13 -0500

    Finally got api gateway to work and retrieve the response from lambda. Not the best way but it works

--- js/index.js
+++ js/index.js
@@ -1,17 +1,49 @@
-var API_URL = "https://rvyl38zfdf.execute-api.us-east-1.amazonaws.com/dev"
+var API_URL = "https://rvyl38zfdf.execute-api.us-east-1.amazonaws.com/prod/"
 
+$(document).ready(function() {
+    $("#submit").on("click", function() {
+        console.log("dfsl");
+        in_data = {
+                    "user": "U",
+                    "password": "p"
+                  }
+        in_date = JSON.stringify(in_data)
+        $.ajax({
+                type: JSON,
+                url: API_URL,
+                data: in_data,
+                contentType: 'connection/json'
+                });
+    })
+});
 
-$("#submit").on("click", function() {
+function validate()
+{
+    jQuery.ajaxSetup({async:false});
     console.log("dfsl");
-    in_data = {
-                "user": ,
-                "password":
-              }
-    in_date = JSON.stringy(in_data)
-    $.ajax({
-            type: JSON,
-            url: API_URL,
-            data: in_data,
-            contentType: 'connection/json'
-            })
-})
+        in_data = {
+                    "username": document.getElementById("usernameInput").value,
+                    "password": document.getElementById("passwordInput").value
+                  }
+        var res =  $.ajax({
+                type: "POST",
+                url: API_URL,
+                data: JSON.stringify(in_data),
+                contentType: 'application/json'
+//                async: false
+                });
+        console.log(res["responseJSON"]['body'])
+
+//    if(   document.getElementById("usernameInput").value == "a"
+//       && document.getElementById("passwordInput").value == "a" )
+//    {
+//        alert( "validation succeeded" );
+//        location.href="profile.html";
+//    }
+//    else
+//    {
+//        alert( "Username or password was incorrect" );
+////        location.href="fail.html";
+//    }
+}
+
