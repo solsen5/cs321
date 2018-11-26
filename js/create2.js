37f469a20598b38bed0a671c944547d5adef722b
Samantha <solsen5@gmu.edu>
2018-11-26 16:29:33 -0500

    change instead of user inputs birthday they now input age

--- js/create2.js
+++ js/create2.js
@@ -4,25 +4,26 @@ var API_URL = "https://rvyl38zfdf.execute-api.us-east-1.amazonaws.com/prod/creat
 jQuery.ajaxSetup({async:false});
 
 function updateUserInformation() {
+    console.log("inside update");
     var username = localStorage.getItem("username");
     var weight = document.getElementById("weight").value
     var gender = document.getElementById("gender").value
     var feet = document.getElementById("feet").value
     var inches = document.getElementById("inches").value
-    var bday = document.getElementById("datePicker").value
+    var age = document.getElementById("age").value
     in_data = {
                 "username": username,
-                "bday" : bday,
+                "age" : age,
                 "gender": gender,
                 "weight": weight,
                 "height": feet + "'" + inches + '"',
                 "part": "2"
               }
-//    var res =  $.ajax({
-//            type: "POST",
-//            url: API_URL,
-//            data: JSON.stringify(in_data),
-//            contentType: 'application/json'
-//            });
+    var res =  $.ajax({
+            type: "POST",
+            url: API_URL,
+            data: JSON.stringify(in_data),
+            contentType: 'application/json'
+            });
 
 }
