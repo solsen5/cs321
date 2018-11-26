74322598c0f6b04829746d2f619656055b971262
Samantha <solsen5@gmu.edu>
2018-11-26 15:46:19 -0500

    Create a javascript for create2 and made changes to create html and create js

---
+++ js/create2.js
@@ -0,0 +1,28 @@
+//Link for the api gateway
+var API_URL = "https://rvyl38zfdf.execute-api.us-east-1.amazonaws.com/prod/createuser"
+//Turns off async so response from lambda can be used client-side
+jQuery.ajaxSetup({async:false});
+
+function updateUserInformation() {
+    var username = localStorage.getItem("username");
+    var weight = document.getElementById("weight").value
+    var gender = document.getElementById("gender").value
+    var feet = document.getElementById("feet").value
+    var inches = document.getElementById("inches").value
+    var bday = document.getElementById("datePicker").value
+    in_data = {
+                "username": username,
+                "bday" : bday,
+                "gender": gender,
+                "weight": weight,
+                "height": feet + "'" + inches + '"',
+                "part": "2"
+              }
+//    var res =  $.ajax({
+//            type: "POST",
+//            url: API_URL,
+//            data: JSON.stringify(in_data),
+//            contentType: 'application/json'
+//            });
+
+}
